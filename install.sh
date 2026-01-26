951c4a6e3dc9f26f4c1ddcd467153359c000704c
dimzachar <zacharenakis@proton.me>
2026-01-26 18:25:44 +0200

    Refactor install.sh for better installation process
    Updated script to improve installation messages and use env bash.

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
 
-# Install oh-my-posh
-curl -s https://ohmyposh.dev/install.sh | bash -s
+echo "Installing oh-my-posh"
 
-# Create theme directory and download theme
-mkdir -p ~/.poshthemes
-curl -o ~/.poshthemes/hul10.omp.json https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/hul10.omp.json
+# install oh-my-posh binary
+curl -fsSL https://ohmyposh.dev/install.sh | bash -s
 
-# Install fzf
-sudo apt-get update && sudo apt-get install -y fzf
+echo "Installing theme"
+
+mkdir -p "$HOME/.poshthemes"
+curl -fsSL \
+  https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/hul10.omp.json \
+  -o "$HOME/.poshthemes/hul10.omp.json"
+
+echo "Done"
