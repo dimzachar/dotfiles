d82d86ee74cfb27dbf58d07e9e94a23090c11c64
dimzachar <zacharenakis@proton.me>
2026-01-27 13:50:12 +0200

    Enhance bash completion in install.sh
    Added programmable completion features for bash.

--- install.sh
+++ install.sh
@@ -18,6 +18,15 @@ if [ -f /etc/bash_completion ]; then
     . /etc/bash_completion
 fi
 
+# Enable programmable completion features
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
+
 # fzf
 if command -v fzf &> /dev/null; then
     eval "$(fzf --bash)"
@@ -26,6 +35,8 @@ fi
 # History
 HISTSIZE=4096
 HISTFILESIZE=4096
+HISTCONTROL=ignoredups:erasedups
+shopt -s histappend
 
 # Aliases
 alias cn='rm -rf .next'
